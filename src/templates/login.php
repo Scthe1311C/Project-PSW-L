902792dffb96fd848273c7f43a114bb4064c8780
Scthe1311C <192221@student.pwr.wroc.pl>
2013-10-25 00:20:12 +0200

    Login page base.
    Create login page base layout. Add dialog classes,

--- src/templates/login.php
+++ src/templates/login.php
@@ -1,25 +1,32 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-	<head>
-		<title><?php echo $title; ?></title>
-
-		<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css"/>
-		<link rel="stylesheet" type="text/css" href="src/css/style.css"/>
-
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	</head>
-	
-	<body>
-		<div class="container">
-			<div class="content">
-
-			<!-- main page content -->
-				<div class="row">
-					login
+    <head>
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="src/css/style.css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    </head>
+    <body>
+        <div id="login-page">
+            <!-- main page content -->
+            <div class="dialog">
+                <header>
+                    <h1>
+                        Dropbox App
+                    </h1>
+                </header>
+				<div class="dialog-content">
+					<form formmethod="post" style="margin-bottom:0px">
+						<fieldset>
+							<input type="text" placeholder="Username..">
+							<input type="password" placeholder="Password..">
+							<div id="dialog-buttons">
+								<span>Sign in</span>
+							</div>
+						</fieldset>
+					</form>
 				</div>
-				
 			</div>
-			
-		</div>
-	</body>
+        </div>
+    </body>
 </html>
