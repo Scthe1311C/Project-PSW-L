3f5404782ce85d0667764021010357424e8ae3d6
Scthe1311C <192221@student.pwr.wroc.pl>
2013-10-25 20:07:24 +0200

    Separate layout css file

--- src/templates/login.php
+++ src/templates/login.php
@@ -4,6 +4,7 @@
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css"/>
         <link rel="stylesheet" type="text/css" href="src/css/style.css"/>
+		<link rel="stylesheet" type="text/css" href="src/css/login.css"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     </head>
     <body>
