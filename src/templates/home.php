e3888f4fa0633cad5d9e60a0ea7fea7ad950402f
Scthe1311C <192221@student.pwr.wroc.pl>
2013-10-25 22:09:32 +0200

    Base sitemap as home view

--- src/templates/home.php
+++ src/templates/home.php
@@ -10,16 +10,28 @@
 	</head>
 	
 	<body>
-		<div class="container">
-			<div class="content">
-
-			<!-- main page content -->
-				<div class="row">
-					HOME
-				</div>
-				
-			</div>
-			
-		</div>
+		<table style="margin:10px auto">
+			<tr>
+				<td style="text-align: center; width:50%">D:</td>
+				<td style="text-align: center; width:50%">M:</td>
+			</tr>
+			<tr>
+				<td style="vertical-align:top">
+					<li><a href="register">Register</a></li>
+					<li><a href="settings">Settings</a></li>
+					<li><a href="galleries">Galleries</a></li>
+					<li><a href="photo">Photo</a></li>
+				</td>
+				<td>
+					<li><a href="login">Login</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="popular">Popular</a></li>
+					<li><a href="upload">Upload</a></li>
+					<li><a href="profile_photos">Profile photos</a></li>
+					<li><a href="profile_galleries">Profile galleries</a></li>
+				</td>
+			</tr>
+		
+		</table>
 	</body>
 </html>
