5248dcb273bd5cf85fddca35edd66514c3e877e0
Scthe1311C <192221@student.pwr.wroc.pl>
2013-10-26 17:39:04 +0200

    Add navigation bar.

--- src/templates/home.php
+++ src/templates/home.php
@@ -9,29 +9,35 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	</head>
 	
-	<body>
-		<table style="margin:10px auto">
-			<tr>
-				<td style="text-align: center; width:50%">D:</td>
-				<td style="text-align: center; width:50%">M:</td>
-			</tr>
-			<tr>
-				<td style="vertical-align:top">
-					<li><a href="register">Register</a></li>
-					<li><a href="settings">Settings</a></li>
-					<li><a href="galleries">Galleries</a></li>
-					<li><a href="photo">Photo</a></li>
-				</td>
-				<td>
-					<li><a href="login">Login</a></li>
-					<li><a href="about">About</a></li>
-					<li><a href="popular">Popular</a></li>
-					<li><a href="upload">Upload</a></li>
-					<li><a href="profile_photos">Profile photos</a></li>
-					<li><a href="profile_galleries">Profile galleries</a></li>
-				</td>
-			</tr>
-		
-		</table>
+	<body style="background:url('src/img/img1.jpg') no-repeat center center fixed !important; height:100%">
+		<div class="container">
+			<?php include 'nav_bar.php' ?>
+			
+			<div class="dialog-content" style="margin-top:10px;">
+				<table style="margin:10px auto">
+					<tr>
+						<td style="text-align: center; width:50%">D:</td>
+						<td style="text-align: center; width:50%">M:</td>
+					</tr>
+					<tr>
+						<td style="vertical-align:top">
+							<li><a href="register">Register</a></li>
+							<li><a href="settings">Settings</a></li>
+							<li><a href="galleries">Galleries</a></li>
+							<li><a href="photo">Photo</a></li>
+						</td>
+						<td>
+							<li><a href="login">Login</a></li>
+							<li><a href="about">About</a></li>
+							<li><a href="popular">Popular</a></li>
+							<li><a href="upload">Upload</a></li>
+							<li><a href="profile_photos">Profile photos</a></li>
+							<li><a href="profile_galleries">Profile galleries</a></li>
+						</td>
+					</tr>
+				
+				</table>
+			<div>
+		</div>
 	</body>
 </html>
