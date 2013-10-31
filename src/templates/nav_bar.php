23c297950c2c669ceabfd425437676ca7ae57a89
Scthe1311C <192221@student.pwr.wroc.pl>
2013-10-31 18:05:14 +0100

    navigation bar - all to the right, add fancy fonts and colors

--- src/templates/nav_bar.php
+++ src/templates/nav_bar.php
@@ -1,6 +1,12 @@
-<?php $minimal_navbar = isset($minimal_navbar) && $minimal_navbar;?>
+<?php
+$minimal_navbar = isset($minimal_navbar) && $minimal_navbar;
+if( !isset( $user_avatar) ) $user_avatar = "src/img/user-icon.jpg";
+$is_logged=true;
+?>
+
 
 <script>
+/* script to scroll to the nav bar at page load */
 $( document ).ready(function() {
 	document.getElementById('navigation-bar').scrollIntoView();
 	// having calculated height write it to the property
@@ -9,39 +15,27 @@ $( document ).ready(function() {
 	$(navBar).css("height", navBarH);
 	
 	<?php if( $minimal_navbar){?>
-			//$(navBar).addClass("navbar-fixed-top");
-			$(navBar).css("position","relative");
+		$(navBar).css("position","relative");
 	<?php } ?>
 });
 
-/*
-function isScrolledIntoView(elem){
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-*/
-
+/* script to fix the nav bar to scroll to the top if the main image is not visible */
 <?php if( !$minimal_navbar){?>
-$(window).scroll(function() {
-	var elem = $('#navigation-bar-image');
-	var navBar = $('#navigation-bar');
-	var docViewTop = $(window).scrollTop();
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-	var navBarH = $(navBar).height();
-	var isTopImageVisible = docViewTop < elemBottom - navBarH;
-	//console.log( "page: "+docViewTop+" ; image: "+elemBottom+"   -> visible: "+ (docViewTop<elemBottom));
-	if( !isTopImageVisible && !$(navBar).hasClass("navbar-fixed-top")){
-		$(navBar).addClass("navbar-fixed-top");
-		$(navBar).css("position","fixed");
-    }else if( isTopImageVisible && $(navBar).hasClass("navbar-fixed-top")){
-		$(navBar).removeClass("navbar-fixed-top");
-		$(navBar).css("position","absolute");
-	}
-});
+	$(window).scroll(function() {
+		var elem = $('#navigation-bar-image');
+		var navBar = $('#navigation-bar');
+		var docViewTop = $(window).scrollTop();
+		var elemBottom = $(elem).offset().top + $(elem).height();
+		var navBarH = $(navBar).height();
+		var isTopImageVisible = docViewTop < elemBottom - navBarH;
+		if( !isTopImageVisible && !$(navBar).hasClass("navbar-fixed-top")){
+			$(navBar).addClass("navbar-fixed-top");
+			$(navBar).css("position","fixed");
+		}else if( isTopImageVisible && $(navBar).hasClass("navbar-fixed-top")){
+			$(navBar).removeClass("navbar-fixed-top");
+			$(navBar).css("position","absolute");
+		}
+	});
 <?php } ?>
 </script>
 
@@ -57,52 +51,68 @@ $(window).scroll(function() {
 			TODO provide home url
 			TODO provide user avatar
 		-->
-		
 		<!-- TODO check if this uri is ok for every page -->
-		<?php if( !isset( $user_avatar) ) $user_avatar = "src/img/user-icon.jpg"; ?>
 		
+		<ul class="nav navbar-nav">
+			<a class="navbar-brand" id="brand" href="<?php echo $main_url; ?>">Dropbox App</a>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			
+			<!-- Explore -->
+			<li class="dropdown">
+				<a href="#" class="navigation-bar-item dropdown-toggle" data-toggle="dropdown">Explore</a>
+				<ul class="dropdown-menu">
+					<li><a href="#" class="navigation-bar-dropdown-item">Galleries</a></li>
+					<li><a href="#" class="navigation-bar-dropdown-item">Popular</a></li>
+				</ul>
+			</li>
 		
-			<ul class="nav navbar-nav">
-				<a class="navbar-brand" id="brand" href="<?php echo $main_url; ?>">Dropbox App</a>
-				<?php if( !$is_logged) { ?>
-					<li><a href="#" class="navigation-bar-item">Sign Up</a></li>
-				<?php } ?>
-				<li class="dropdown">
-					<a href="#" class="navigation-bar-item dropdown-toggle" data-toggle="dropdown">Explore</a>
+			<!-- Upload -->
+			<?php if( $is_logged) { ?>
+				<li><a href="#" class="navigation-bar-item">Upload</a></li>
+			<?php } ?>
+			
+			<!-- About -->
+			<li><a href="#" class="navigation-bar-item">About</a></li>
+		
+			<!-- Spacer -->
+			<li><span id="navigation-bar-spacer">|</span></li>
+		
+			<!-- Sign In/Up ; user -->
+			<?php if( ! $is_logged) { ?>
+				<li><a href="#" class="navigation-bar-item">Sign Up</a></li>
+				<li><a href="#" class="navigation-bar-item">Sign in</a></li>
+			<?php } else { ?>
+				<!-- user avatar-->
+				<li>
+				<div style="position:relative;">
+					<div id="navigation-bar-avatar"></div>
+				</div>
+				</li>
+				<!-- user pages -->
+				<li class="dropdown" id="navigation-bar-user">
+					<a href="#" class="navigation-bar-item dropdown-toggle" data-toggle="dropdown">
+						<?php echo $user_name; ?>
+					</a>
 					<ul class="dropdown-menu">
-						<li><a href="#" class="navigation-bar-dropdown-item">Galleries</a></li>
-						<li><a href="#" class="navigation-bar-dropdown-item">Popular</a></li>
+						<li><a href="#" class="navigation-bar-dropdown-item">Settings</a></li>
+						<li><a href="#" class="navigation-bar-dropdown-item">My photos</a></li>
+						<li><a href="#" class="navigation-bar-dropdown-item">My galleries</a></li>
 					</ul>
 				</li>
-				<?php if( $is_logged) { ?>
-					<li><a href="#" class="navigation-bar-item">Upload</a></li>
-				<?php } ?>
-			</ul>
-			
-			<ul class="nav navbar-nav navbar-right">
-				<!-- 
-				<input class="navbar-search" id="navigation-bar-search" type="text"> </input>
-				<div class="navigation-bar-search-icon">
-					<input type="submit" name="" value="">
-				</div>
-				 -->
-				<li><a href="#" class="navigation-bar-item">About</a></li>
-				<?php if( ! $is_logged) { ?>
-				<li><a href="#" class="navigation-bar-item">Sign in</a></li>
-				<?php } else { ?>
-					<li class="dropdown">
-						<a href="#" class="navigation-bar-item dropdown-toggle" data-toggle="dropdown">
-							<img src="<?php echo $user_avatar; ?>" width="20" height="20"/>
-							<?php echo $user_name; ?>
-						</a>
-						<ul class="dropdown-menu">
-							<li><a href="#" class="navigation-bar-dropdown-item">Settings</a></li>
-							<li><a href="#" class="navigation-bar-dropdown-item">My photos</a></li>
-							<li><a href="#" class="navigation-bar-dropdown-item">My galleries</a></li>
-						</ul>
-					</li>
-				<?php } ?>
-			</ul>
+			<?php } ?>
+		
+		
+		
+		
+			<!-- 
+			<input class="navbar-search" id="navigation-bar-search" type="text"> </input>
+			<div class="navigation-bar-search-icon">
+				<input type="submit" name="" value="">
+			</div>
+			 -->
+		
+		</ul>
 			
 		
 		
